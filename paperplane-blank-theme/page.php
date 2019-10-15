c72d3b9e44e53659d23be21c7e9157077759dcef
paperplanefactory <giovanni@paperplanefactory.com>
2019-10-15 12:42:48 +0200

    Updates October 2019
    Changed folders structure
    Replaced BX Slider with Slick Slider
    Optimized Lighthouse tests

--- paperplane-blank-theme/page.php
+++ paperplane-blank-theme/page.php
@@ -25,7 +25,8 @@ get_header();
     <div class="wrapper-padded">
       <div class="wrapper-padded-more">
         <div class="wrapper-padded-more-840">
-          <div class="content-styled">
+          <div class="content-styled modulo-space">
+            <?php paperplane_breadcrumbs(); ?>
             <h1><?php the_title(); ?></h1>
             <?php the_content(); ?>
           </div>
@@ -39,44 +40,6 @@ get_header();
 
 
 
-<h2>griglia a 4 - margins-thin</h2>
-
-
-
-<div class="flex-hold flex-hold-4 margins-thin bg-5">
-  <?php
-  // query con tassonomia
-  $args_filter_posts = array(
-    'post_type' => 'post',
-    'posts_per_page' => -1
-  );
-  $my_filter_posts = get_posts( $args_filter_posts );
-  ?>
-  <?php if ( !empty ( $my_filter_posts ) ) : ?>
-    <?php foreach ( $my_filter_posts as $post ) : setup_postdata ($post ); ?>
-      <div class="flex-hold-child bg-3">
-        <?php
-        $image_data = array(
-            'image_type' => 'post_thumbnail', // options: post_thumbnail, acf_field, acf_sub_field
-            'image_value' => 'immagine_acf', // se utilizzi un custom field indica qui il nome del campo
-            'size_fallback' => 'full_desk'
-        );
-        $image_sizes = array( // qui sono definiti i ritagli o dimensioni. Devono corrispondere per numero a quanto dedinfito nella funzione nei parametri data-srcset o srcset
-            'retina' => 'full_desk_retina',
-            'desktop' => 'full_desk',
-            'mobile' => 'content_picture',
-            'micro' => 'micro'
-        );
-        print_theme_image( $image_data, $image_sizes );
-        ?>
-        <h2><?php the_title(); ?></h2>
-      </div>
-    <?php endforeach; wp_reset_postdata(); endif; ?>
-
-
-
-
-</div>
 
 
 
--- paperplane-blank-theme/template-parts/grid/infinite-message.php
+++ paperplane-blank-theme/template-parts/grid/infinite-message.php
@@ -1,17 +1,25 @@
-<div id="infscr-loading">
+<div id="infscr-loading" class="last-child-no-margin">
   <div class="infinite-scroll-request">
+    <div class="spinner">
+      <div class="bounce1 bg-2"></div>
+      <div class="bounce2 bg-2"></div>
+      <div class="bounce3 bg-2"></div>
+    </div>
     <p>
-      <?php pll_e('morecontents_output'); ?>
+      loading more contents
+      <?php //pll_e('morecontents_output'); ?>
     </p>
   </div>
   <div class="infinite-scroll-last">
     <p>
-      <?php pll_e('nomorecontents_output'); ?>
+      no more contents
+      <?php //pll_e('nomorecontents_output'); ?>
     </p>
   </div>
   <div class="infinite-scroll-error">
     <p>
-      <?php pll_e('ops_output'); ?>
+      ops, an error has occurred
+      <?php //pll_e('ops_output'); ?>
     </p>
   </div>
 </div>
