490adefa9f104e1c0f9cc539b036b480296772c1
paperplanefactory <giovanni@paperplanefactory.com>
2020-02-06 15:13:24 +0100

    Damn me!
    Scripts path set with get_stylesheet_directory_uri instead of get_template_directory_uri

--- includes/theme-scriptsloader.php
+++ includes/theme-scriptsloader.php
@@ -29,7 +29,7 @@ function all_scripts(){
   // smart jquery inclusion
   if (!is_admin()) {
   	wp_deregister_script('jquery');
-  	wp_register_script('jquery', get_stylesheet_directory_uri() . '/assets/js/libs/jquery-3.4.1.min.js', '', '3.4.1', false);
+  	wp_register_script('jquery', get_template_directory_uri() . '/assets/js/libs/jquery-3.4.1.min.js', '', '3.4.1', false);
   	wp_enqueue_script('jquery');
   }
   // Infinite Scroll
@@ -39,22 +39,22 @@ function all_scripts(){
 
   // Lazy load
   // documentazione: http://www.andreaverlicchi.eu/lazyload/
-  wp_register_script( 'vanilla-lazyload', get_stylesheet_directory_uri() . '/assets/js/libs/lazyload.min.js#deferload', '', '12.0.0', true);
+  wp_register_script( 'vanilla-lazyload', get_template_directory_uri() . '/assets/js/libs/lazyload.min.js#deferload', '', '12.0.0', true);
   wp_enqueue_script( 'vanilla-lazyload' );
 
   // AOS
   // documentazione: https://github.com/michalsnik/aos
-  wp_register_script( 'theme-aos', get_stylesheet_directory_uri() . '/assets/js/libs/aos.min.js#deferload', '', $theme_version, true);
+  wp_register_script( 'theme-aos', get_template_directory_uri() . '/assets/js/libs/aos.min.js#deferload', '', $theme_version, true);
   wp_enqueue_script( 'theme-aos' );
 
   // slick
   // documentazione: https://github.com/kenwheeler/slick
-  wp_register_script( 'slick', get_stylesheet_directory_uri() . '/assets/js/libs/slick.min.js#deferload', '', $theme_version, false);
+  wp_register_script( 'slick', get_template_directory_uri() . '/assets/js/libs/slick.min.js#deferload', '', $theme_version, false);
 	wp_enqueue_script( 'slick' );
 
   // parallasse
   // documentazione: https://github.com/dixonandmoe/rellax
-	// wp_register_script( 'js-parallax', get_stylesheet_directory_uri() . '/assets/js/libs/rellax.min.js', '', '1.10.0', false);
+	// wp_register_script( 'js-parallax', get_template_directory_uri() . '/assets/js/libs/rellax.min.js', '', '1.10.0', false);
 	// wp_enqueue_script( 'js-parallax' );
 
   // FontAwesome
@@ -63,6 +63,6 @@ function all_scripts(){
 	// wp_enqueue_script( 'theme-fontawesome' );
 
   // Comportamenti ricorrenti
-	wp_register_script( 'theme-general', get_stylesheet_directory_uri() . '/assets/js/theme-general.min.js#deferload', '', $theme_version, true);
+	wp_register_script( 'theme-general', get_template_directory_uri() . '/assets/js/theme-general.min.js#deferload', '', $theme_version, true);
 	wp_enqueue_script( 'theme-general' );
 }
