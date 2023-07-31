2efab18933f4a65a00d6522eac98b45a1cc754c5
paperplanefactory <giovanni@paperplanefactory.com>
2023-07-31 12:18:04 +0200

    Aggiunto generatore pagina under construction + access. form

--- includes/theme-maintenance.php
+++ includes/theme-maintenance.php
@@ -7,10 +7,18 @@ function paperplane_set_maintenance_page() {
 	$favicons_folder = get_stylesheet_directory_uri() . '/assets/images/favicons/';
 	$maintenance_message = get_field( 'maintenance_message', 'option' );
 	if ( $maintenance_message ) {
-		$message = $maintenance_message;
+		$message_m = $maintenance_message;
 	} else {
-		$message = 'Sito in manutenzione - torneremo online in un minuto.<br />
+		$message_m = 'Sito in manutenzione - torneremo online in un minuto.<br />
       Site under maintenance - we will be back online in a minute.';
+
+	}
+	$underconstruction_message = get_field( 'underconstruction_message', 'option' );
+	if ( $underconstruction_message ) {
+		$message_u = $maintenance_message;
+	} else {
+		$message_u = 'Sito in costruzione.<br />
+    Under construction.';
 
 	}
 	$offline_page_data = '<!doctype html>
@@ -71,7 +79,7 @@ function paperplane_set_maintenance_page() {
   <div class="content">
   <img src="' . parse_url( get_stylesheet_directory_uri(), PHP_URL_PATH ) . '/assets/images/site-logo-header.svg" />
   <h1>
-  ' . $message . '
+  ' . $message_m . '
   </h1>
   </div>
   </div>
@@ -80,6 +88,89 @@ function paperplane_set_maintenance_page() {
 	$offline_page_file = '/../../../maintenance.php';
 	// write/overwrite manifest.json on site root folder
 	file_put_contents( __DIR__ . $offline_page_file, $offline_page_data );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$underconstruction_page_data = '<!doctype html>
+  <html lang="it">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>' . get_bloginfo( 'name' ) . ' | ' . get_bloginfo( 'description' ) . '</title>
+  <meta name="description" content="' . get_bloginfo( 'name' ) . ' | ' . get_bloginfo( 'description' ) . '">
+  <meta name="author" content="' . get_bloginfo( 'name' ) . '">
+  <meta name="robots" content="noindex, nofollow" />
+  <style>
+  body {
+    background-color: ' . $bg_color . ';
+    color: ' . $color . ';
+    font-family:Arial, Helvetica, sans-serif;
+  }
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  .offline-page {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+    min-height: 100dvh;
+    display: grid;
+    padding: 0;
+    margin: 0;
+    align-content: center;
+    width: 100%;
+  }
+  .content {
+    position: relative;
+    text-align: center;
+    max-width: 750px;
+    margin: 0 auto;
+    padding: 4vw;
+  }
+
+  .content img {
+    max-width: 200px;
+    height: auto;
+    margin: 0 0 30px 0;
+  }
+
+  h1 {
+    font-size: 20px;
+    line-height: 28px;
+  }
+  </style>
+  </head>
+  <body>
+  <div class="offline-page">
+  <div class="content">
+  <img src="' . parse_url( get_stylesheet_directory_uri(), PHP_URL_PATH ) . '/assets/images/site-logo-header.svg" />
+  <h1>
+  ' . $message_u . '
+  </h1>
+  </div>
+  </div>
+  </body>
+  </html>';
+	$underconstruction_page_file = '/../under-construction.php';
+	// write/overwrite manifest.json on site root folder
+	file_put_contents( __DIR__ . $underconstruction_page_file, $underconstruction_page_data );
 }
 
 
--- under-construction.php
+++ under-construction.php
@@ -1,81 +1,65 @@
 <!doctype html>
-<html lang="it">
+  <html lang="it">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PaperPlane: BlankPaper | Our WordPress Starter Theme</title>
+  <meta name="description" content="PaperPlane: BlankPaper | Our WordPress Starter Theme">
+  <meta name="author" content="PaperPlane: BlankPaper">
+  <meta name="robots" content="noindex, nofollow" />
+  <style>
+  body {
+    background-color: #FFFFFF;
+    color: #000000;
+    font-family:Arial, Helvetica, sans-serif;
+  }
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  .offline-page {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+    min-height: 100dvh;
+    display: grid;
+    padding: 0;
+    margin: 0;
+    align-content: center;
+    width: 100%;
+  }
+  .content {
+    position: relative;
+    text-align: center;
+    max-width: 750px;
+    margin: 0 auto;
+    padding: 4vw;
+  }
 
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  .content img {
+    max-width: 200px;
+    height: auto;
+    margin: 0 0 30px 0;
+  }
 
-	<title>Komo | Fusionable Taste</title>
-	<meta name="description" content="Komo | Fusionable Taste">
-	<meta name="author" content="Komo">
-
-	<meta property="og:title" content="Komo | Fusionable Taste">
-	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://komo-restaurant.it/">
-	<meta property="og:description" content="Komo | Fusionable Taste">
-	<meta property="og:image" content="https://komo-restaurant.it/splash-page/images/splash-image.png">
-	<link rel="stylesheet" href="https://use.typekit.net/xeu5uvn.css">
-	<link rel="icon" href="https://komo-restaurant.it/splash-page/images/favicon.ico">
-	<link rel="icon" href="https://komo-restaurant.it/splash-page/images/favicon.svg" type="image/svg+xml">
-	<link rel="apple-touch-icon" sizes="57x57"
-		href="https://komo-restaurant.it/splash-page/images/favicons/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="https://komo-restaurant.it/splash-page/images/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="https://komo-restaurant.it/splash-page/images/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="https://komo-restaurant.it/splash-page/images/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114"
-		href="https://komo-restaurant.it/splash-page/images/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120"
-		href="https://komo-restaurant.it/splash-page/images/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144"
-		href="https://komo-restaurant.it/splash-page/images/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152"
-		href="https://komo-restaurant.it/splash-page/images/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180"
-		href="https://komo-restaurant.it/splash-page/images/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"
-		href="https://komo-restaurant.it/splash-page/images/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32"
-		href="https://komo-restaurant.it/splash-page/images/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96"
-		href="https://komo-restaurant.it/splash-page/images/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16"
-		href="https://komo-restaurant.it/splash-page/images/favicon-16x16.png">
-	<meta name="msapplication-TileColor" content="#A8926A">
-	<meta name="msapplication-TileImage"
-		content="https://komo-restaurant.it/splash-page/images/favicons/ms-icon-144x144.png">
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-	<!-- Chrome, Firefox OS and Opera -->
-	<meta name="theme-color" content="#1C1511">
-	<!-- Windows Phone -->
-	<meta name="msapplication-navbutton-color" content="#1C1511">
-	<!-- iOS Safari -->
-	<meta name="apple-mobile-web-app-status-bar-style" content="#1C1511">
-	<link rel="stylesheet" href="https://komo-restaurant.it/splash-page/css/style.min.css">
-	<link rel="stylesheet" href="https://use.typekit.net/mce0yly.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-</head>
-
-<body class="unloaded">
-	<div class="container">
-		<div class="with-image"></div>
-	</div>
-	<div class="content">
-		<div class="logo">
-			<img src="https://komo-restaurant.it/splash-page/images/favicon.svg" alt="logo Komo">
-		</div>
-		<p>
-			<a target="_blank"
-				href="https://www.google.it/maps/place/KOMO+Fusionable+Taste/@45.8112452,9.0775813,17z/data=!3m1!4b1!4m5!3m4!1s0x47869d5c933b0111:0x4c57d802b8b0b72b!8m2!3d45.8112247!4d9.0797327">
-				Via Paolo Carcano 15, 22100 Como </a>
-			| <a class="cell" href="tel: +39 0316124676"> Tel. +39 031 612 4676 </a>
-		</p>
-	</div>
-	<script type="text/javascript">
-		jQuery(document).ready(function () {
-			jQuery('body').removeClass('unloaded');
-		});
-	</script>
-</body>
-
-</html>
+  h1 {
+    font-size: 20px;
+    line-height: 28px;
+  }
+  </style>
+  </head>
+  <body>
+  <div class="offline-page">
+  <div class="content">
+  <img src="/wp-content/themes/paperplane-blank-theme/assets/images/site-logo-header.svg" />
+  <h1>
+  Sito in costruzione.<br />
+    Under construction.
+  </h1>
+  </div>
+  </div>
+  </body>
+  </html>
